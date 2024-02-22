9a416bb3ea0c7b179951f014a931dd0a700f5d80
mascus007 <sirdanny8@gmail.com>
2024-02-22 11:15:19 +0000

    fixed mobile responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import DashboardView from "@/features/Dashboard";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-10">
+    <main className="flex min-h-screen flex-col p-5 xl:p-10 md:p-10 lg:p-10">
         <DashboardView/>
     </main>
   );
